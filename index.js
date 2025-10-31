50b4a41c94e1c59ab1ffa838df770f0019ef8f34
worker300 <munnabavkar12345@gmail.com>
2025-10-31 21:52:08 +0530

    second

---
+++ index.js
@@ -0,0 +1,16 @@
+import jsonfile from 'jsonfile';
+import moment from 'moment';
+import simpleGit from 'simple-git';
+
+
+const path = "/data.json";
+const date = moment().format();
+
+const data = {
+    "dete": date, 
+}
+
+jsonfile.writeFile(path , data);
+
+simpleGit().add([path]).commit(date ,{'--date':date}).push();
+
